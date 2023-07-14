c0c8c09f7d0417eb81ce37ce2036d98e22ecad3c
Nikoloz Naskidashvili <nikoloz2005@gmail.com>
2023-07-14 16:23:30 +0400

    [back & front] feat: websocket initial setup

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+use actix::{Actor, StreamHandler};
+use actix_web::{get, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web_actors::ws;
+
+struct MyWs;
+
+impl Actor for MyWs {
+    type Context = ws::WebsocketContext<Self>;
+}
+
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWs {
+    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
+        match msg {
+            Ok(ws::Message::Text(text)) => ctx.text(text),
+            _ => (),
+        }
+    }
+}
+
+async fn ws_index(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
+    let resp = ws::start(MyWs {}, &req, stream);
+    println!("{:?}", resp);
+
+    resp
+}
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Welcome to Rusty Chat Backend!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+            .route("/ws/", web::get().to(ws_index))
+    })
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
--- frontend/src/components/ChatWindow/ChatWindow.tsx
+++ frontend/src/components/ChatWindow/ChatWindow.tsx
@@ -1,50 +1,96 @@
-import { useState } from 'react';
+import { useState, useCallback, useEffect } from 'react';
+import useWebSocket, { ReadyState } from 'react-use-websocket';
+
+import { Card, Input, Text } from "@nextui-org/react";
 
 import "./ChatWindow.scss"
 
 const ChatWindow = () => {
   const [inputValue, setInputValue] = useState('');
+  const [messageHistory, setMessageHistory] = useState([]);
+
+  const { sendMessage, lastMessage, readyState } = useWebSocket('ws://127.0.0.1:8080/ws/');
+
+  const connectionStatus = {
+    [ReadyState.CONNECTING]: 'connecting ...',
+    [ReadyState.OPEN]: 'open',
+    [ReadyState.CLOSING]: 'closing',
+    [ReadyState.CLOSED]: 'closed',
+    [ReadyState.UNINSTANTIATED]: 'uninstantiated',
+  }[readyState];
+
+  useEffect(() => {
+    if (lastMessage !== null) {
+      setMessageHistory((prev) => prev.concat(lastMessage.data));
+    }
+  }, [lastMessage, setMessageHistory]);
 
   const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Enter') {
       event.preventDefault();
-      document.getElementById('sendButton')?.click();
+      handleSendMessage();
     }
   };
 
-  const handleButtonClick = () => {
-    console.log(inputValue);
-  };
+  const handleSendMessage = useCallback(() => {
+    sendMessage(inputValue);
+    setInputValue('');
+  }, [inputValue, sendMessage]);
 
   return (
-    <div className="chat_window">
-      <div className="chat_message_box">
-        <div className="chat_message">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-        </div>
-        <div className="chat_message">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis ad, nesciunt dolorem accusamus saepe rem 
-        </div>
-        <div className="chat_message">
-          Sure! I'll send you a link.
-        </div>
+    <Card css={{ mw: "400px" }}>
+      <Card.Header
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: "0.2rem",
+        }}
+      >
+        <Text size="$xs">The WebSocket is currently {connectionStatus}</Text>
+      </Card.Header>
+      <Card.Body>
+        <div className="chat_message_box">
+          {messageHistory.map((message: string, idx) => (
+            <div className="chat_message" key={idx}>
+              {message.toString()} 
+            </div>
+          ))}
       </div>
-      <div className="chat_message_input">
-        <input
-          type="text"
+      </Card.Body>
+      <Card.Footer>
+        <Input 
+          bordered
+          width='100%'
+          size='sm'
           placeholder="Type a message..."
+          color="primary" 
           value={inputValue}
           onChange={(event) => setInputValue(event.target.value)}
-          onKeyPress={handleKeyPress}   
+          onKeyPress={handleKeyPress}
+          disabled={readyState !== ReadyState.OPEN} 
         />
-        <button
-          id="sendButton"
-          onClick={handleButtonClick}
-        >
-          <i className="fa-solid fa-paper-plane"></i>
-        </button> 
-      </div>
-    </div>
+      </Card.Footer>
+    </Card>
+
+    // <div className="chat_window">
+    //   <span>The WebSocket is currently {connectionStatus}</span>
+
+    //   <div className="chat_message_box">
+
+        
+    //   </div>
+    //   <div className="chat_message_input">
+    //     <input
+    //       type="text"
+    //       placeholder="Type a message..."
+    //       value={inputValue}
+    //       onChange={(event) => setInputValue(event.target.value)}
+    //       onKeyPress={handleKeyPress}
+    //       disabled={readyState !== ReadyState.OPEN}   
+    //     />
+    //   </div>
+    // </div>
   )
 }
 
